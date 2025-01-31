536221b35dad64dfd24eafbba40eb9f63f7c9abd
Marco Hartz <marcoavhu@gmail.com>
2025-01-31 08:20:56 -0300

    navbar, fonts, animations

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from 'next-themes';
+
+
+const font = DM_Sans({ subsets: ['latin']})
+
+export const metadata: Metadata = {
+  title: "Weavier",
+  description: "Automatize seu trabalho com Weavier",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+
+      </body>
+    </html>
+  );
+}
